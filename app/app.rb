6bbe4d4732910594938f7acd1d074d74d00bdb6c
paulwallis42 <paulwallis42@gmail.com>
2017-02-09 20:13:36 +0000

    rake file updated

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,8 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
-ENV['RACK_ENV'] ||= 'development'
 
 class GolfTracker < Sinatra::Base
   get '/' do
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,11 +1,10 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
-require './app/app.rb'
 
 require_relative 'models/user'
 require_relative 'models/round'
 require_relative 'models/hole'
 
-DataMapper.setup(:default,  ENV['DATABASE_URL'] || "postgres://localhost/golf_project_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/golf_project_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
