deae22187d9bd5b2fd598b3b81a3106756f52c77
paulwallis42 <paulwallis42@gmail.com>
2017-02-09 17:24:53 +0000

    holes tests passing

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -1,3 +1,11 @@
 class Hole
+  include DataMapper::Resource
+
+  property :id,       Serial
+  property :par,      Integer
+  property :si,       Integer
+  property :distance, Integer
+  property :putts,    Integer
+  property :shots,    Integer
 
 end
