9540187617c3f69ad5334d5a5dda95c3ae85b89e
paulwallis42 <paulwallis42@gmail.com>
2017-02-22 15:57:40 +0000

    competition parent class created and stroke, three_putt and stableford changed to subclasses

---
+++ app/lib/competition.rb
@@ -0,0 +1,24 @@
+class Competition
+
+  def self.winner(all_rounds)
+    @all_rounds = all_rounds
+    @multi_round_date = get_round_date[0]
+    @selected_rounds = get_multi_rounds
+    get_winner
+  end
+
+  def self.get_round_date
+    rounds_array = @all_rounds.aggregate(:date, :all.count)
+    rounds_array.reverse!
+    rounds_array.each do |round|
+      if round[1] > 1
+        return round
+      end
+    end
+  end
+
+  def self.get_multi_rounds
+    @all_rounds.all(date: @multi_round_date)
+  end
+  
+end
--- app/lib/stroke_play_comp.rb
+++ app/lib/stroke_play_comp.rb
@@ -1,25 +1,6 @@
-class StrokePlayComp
+require_relative 'competition'
 
-  def self.winner(all_rounds)
-    @all_rounds = all_rounds
-    @multi_round_date = get_round_date[0]
-    @selected_rounds = get_multi_rounds
-    get_winner
-  end
-
-  def self.get_round_date
-    rounds_array = @all_rounds.aggregate(:date, :all.count)
-    rounds_array.reverse!
-    rounds_array.each do |round|
-      if round[1] > 1
-        return round
-      end
-    end
-  end
-
-  def self.get_multi_rounds
-    @all_rounds.all(date: @multi_round_date)
-  end
+class StrokePlayComp < Competition
 
   def self.get_winner
     @selected_rounds.min_by do |round|
